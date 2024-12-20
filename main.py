87431999ff445594241cbb7cd537a5479249dbb2
Roip <roip.tkshs@gmail.com>
2024-12-20 20:43:20 +0200

    Added base pygame project

--- main.py
+++ main.py
@@ -1,5 +1,41 @@
+__author__ = "Ido Senn and the Roip team"
+
+import sys
+
+from settings import *
+import sys
+
+
+class App:
+    def __init__(self):
+        pg.init()
+        pg.display.set_caption(WINDOW_CAPTION)
+        self.screen = pg.display.set_mode(RES)
+        self.clock = pg.time.Clock()
+
+    def update(self):
+        self.clock.tick(FPS)
+
+    def draw(self):
+        self.screen.fill(color=BG_COLOR)
+        pg.display.flip()
+
+    def check_events(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pg.quit()
+                sys.exit()
+
+    def run(self):
+        while True:
+            self.check_events()
+            self.update()
+            self.draw()
+
+
 def main():
-    print("Hello World!")
+    app = App()
+    app.run()
 
 
 if __name__ == "__main__":
---
+++ settings.py
@@ -0,0 +1,13 @@
+import pygame as pg
+
+
+WINDOW_CAPTION = "Yarin Quest: Copyright Infringement"
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 450
+RES = WINDOW_WIDTH, WINDOW_HEIGHT
+
+
+BG_COLOR = (24, 89, 117)
+
+
+FPS = 60
